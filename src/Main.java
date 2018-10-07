74f0e777d8ce452819f088bfe4f5f5b602614ae4
DanOliverRunoLarsen <42993978+DanOliverRunoLarsen@users.noreply.github.com>
2018-10-07 18:01:45 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int marks[] = {10,12,7,4,02};
+        String subjects[] = {"Engelsk","Tysk","Fransk","Dansk","Programering"};
+
+        for (int count = 0; count < marks.length;count++)
+        {
+            System.out.print(subjects[count]+ " = ");
+            System.out.println(marks[count]);
+    }
+        System.out.println("The sum is " + (marks[0]+marks[1]+marks[2]+marks[3]+marks[4]));
+}}
---
+++ src/Opg7_1.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Arrays;
+
+
+public class Opg7_1 {
+    public static void main(String[] args) {
+        System.out.print("Enter number of students: ");
+        Scanner sc = new Scanner(System.in);
+        int students = sc.nextInt();
+        System.out.println();
+        int[] scores = new int[students];
+        for (int l = 0; l < scores.length; l++)
+        { System.out.print("Enter " + students + " scores: ");
+        scores[l]=sc.nextInt();}
+
+        Arrays.sort(scores);
+        String[] grade = new String[scores.length];
+        int bestScore = scores[(scores.length-1)];
+        System.out.println(bestScore);
+        for (int i = 0;i<students;i++)
+        {if (scores[i] >= bestScore - 10)
+            {grade[i]= "A";}
+        else if (scores[i] >= bestScore -20)
+        {grade[i] = "B";}
+        else if (scores[i] >= bestScore -30)
+        {grade[i] = "C";}
+        else if (scores[i] >= bestScore -40)
+        {grade[i] = "D";}
+        else
+        {grade[i] = "F";}
+        }
+
+        for (int p = 0; p < students;p++)
+        {
+        System.out.println("The student with the score of " + scores[p] + " got the grade: " + grade [p]);}
+
+
+    }
+
+
+}
